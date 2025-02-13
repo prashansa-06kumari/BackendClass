05575d8c28b17addfff22a8e0af874a4a972df82
prashansa-06kumari <158124679+prashansa-06kumari@users.noreply.github.com>
2025-02-13 11:49:25 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app=express();
+
+// app.get("/profile",(req,res)=>{
+//     const {username,age} = req.query;
+//     console.log(username);
+//     res.send("profile page of"+" "+username+" "+age)
+// })
+
+// app.get("/profile/:id/:username",(req,res)=>{
+//     const {id,username} = req.params;
+//     //find in database
+//     res.send("profile page of" + " "+id+" "+username)
+// })
+
+app.listen(2800,(req,res)=>{
+    console.log("server started");
+})
+
+
+let usersData=[
+    {
+        id:1,
+        name:"Nitesh",
+        address:"Delhi"
+    },
+    {
+        id:2,
+        name:"Ritik",
+        address:"Faridabad"
+    },
+    {
+        id:3,
+        name:"Neha",
+        address:"Punjab"
+    },
+]
+app.get("/allusers",(req,res)=>{
+    res.send(usersData);
+})
+
+app.get("/getuserbyId",(req,res)=>{
+    // const id = req.query.id
+    const {id} = req.query;
+    for(let i=0;i<usersData.length;i++){
+        if(usersData[i].id==id){
+            return res.send(usersData[i])
+        }
+    }
+    res.send("User not found")
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
